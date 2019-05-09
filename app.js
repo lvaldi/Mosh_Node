31215351d61bff43cb3e1836c42b0eb397d446d1
Darren Fu <darrenfu98@gmail.com>
2019-05-08 22:15:17 -0700

    First commit

---
+++ app.js
@@ -0,0 +1,27 @@
+//
+// function sayHello(name) {
+//     console.log('Hello ' + name)
+// }
+//
+// // sayHello('World');
+//
+// console.log(); // global
+//
+// setTimeout(); // delay
+// clearTimeout();
+//
+// setInterval(); // call function repetitively with delay
+// clearInterval();
+//
+// var message = '';
+//
+// console.log(global.message);
+//
+// var sayHello = function() {
+//
+// }
+//
+// window.sayHello();
+
+var logger require('./logger');
+logger.log('message');
