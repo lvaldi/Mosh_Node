590948816560ac94bde40b19624338cd0acd25ac
Darren Fu <darrenfu98@gmail.com>
2019-05-09 23:58:03 -0700

    OS Module

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 
-const path = require('path');
-var pathObj = path.parse(__filename);
+const os = require('os');
 
-console.log(pathObj);
+var totalMemory = os.totalmem();
+var freeMemory = os.freemem();
+
+// console.log('Total Memory: ' + totalMemory);
+
+// Template string
+// ES6 / ES2015 : ECMAScript 6
+
+console.log(`Total Memory: ${totalMemory}`);
+console.log(`Free Memory: ${freeMemory}`);
